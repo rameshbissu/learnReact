307abd558e2d5d843f22ef9a4d4ad33c2fb4f918
rameshbissu <rameshbissu646@gmail.com>
2023-12-18 16:47:25 +0530

    jsx rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <>
+      <div className="someValue">
+        <h3>hello people</h3>
+        <ul>
+          <li>
+            <a href="#">hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+      <input type="text" name="" id="" />
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
