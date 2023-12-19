99413f83aa95731405217d30e3b2a93bb96713e5
rameshbissu <rameshbissu646@gmail.com>
2023-12-19 15:59:36 +0530

    JSX Javascript

--- src/index.js
+++ src/index.js
@@ -14,27 +14,18 @@ const BookList = () => {
     </section>
   );
 };
-
+const author = "James Clear";
 const Book = () => {
+  const title = "Atomic Habits";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="./images/book.jpg" alt="Atomic Habits" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
 
-const Image = () => <img src="./images/book.jpg" alt="Atomic Habits" />;
-const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d99",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
-};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
