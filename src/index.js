57acbb0b2557757b4213e158b04bcceac40c0ef9
rameshbissu <rameshbissu646@gmail.com>
2023-12-19 11:39:54 +0530

    Booklist and css

--- src/index.js
+++ src/index.js
@@ -2,20 +2,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
-}
+import "./index.css";
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 };
 
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/91bYsX41DVL._SY342_.jpg"
+    alt="Atomic Habits"
+  />
+);
+const Title = () => <h2>Atomic Habits</h2>;
+const Author = () => {
+  return <h4>James Clear</h4>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
