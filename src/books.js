725181e50791bf92b14ba7ef5c9e1b577babb293
rameshbissu <rameshbissu646@gmail.com>
2023-12-20 22:19:05 +0530

    ES6 Modules

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "./images/book.jpg",
+    id: 1,
+  },
+  {
+    author: "Raghuram Rajan",
+    title: "Breaking the Mould : Reimagining India's Economic Future",
+    img: "https://m.media-amazon.com/images/I/71DEKXIsW8L._SY342_.jpg",
+    id: 2,
+  },
+];
+// export default books;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
-const books = [
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "./images/book.jpg",
-    id: 1,
-  },
-  {
-    author: "Raghuram Rajan",
-    title: "Breaking the Mould : Reimagining India's Economic Future",
-    img: "https://m.media-amazon.com/images/I/71DEKXIsW8L._SY342_.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-  // getBook(2);
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  // alert(props);
-  const getSingleBook = () => {
-    getBook(id);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>click me</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
