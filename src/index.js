159e7d36b28a530fbe23c8dbccecbf85931a227b
rameshbissu <rameshbissu646@gmail.com>
2023-12-20 12:12:58 +0530

    simple list

--- src/index.js
+++ src/index.js
@@ -3,41 +3,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const firstBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "./images/book.jpg",
-};
-const SecondBook = {
-  author: "Raghuram Rajan",
-  title: "Breaking the Mould : Reimagining India's Economic Future",
-  img: "https://m.media-amazon.com/images/I/71DEKXIsW8L._SY342_.jpg",
-};
-
+const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "./images/book.jpg",
+  },
+  {
+    author: "Raghuram Rajan",
+    title: "Breaking the Mould : Reimagining India's Economic Future",
+    img: "https://m.media-amazon.com/images/I/71DEKXIsW8L._SY342_.jpg",
+  },
+];
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          lorem, ipsum dolor sit amet consectetur adipisicing elit, Odio fugiat
-          qui officiss indiduct ullam aliwum quae corroupti.
-        </p>
-        <button>click me</button>
-      </Book>
-
-      <Book
-        author={SecondBook.author}
-        title={SecondBook.title}
-        img={SecondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author }) => {
   // console.log(props);
   // const { img, title, author, children } = props;
   return (
@@ -45,7 +31,6 @@ const Book = ({ img, title, author, children }) => {
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
